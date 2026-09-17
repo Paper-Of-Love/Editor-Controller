399f112d4f70690d6963600b5908c7bf134b5e27
Quin Nordmark <quinnordmark@gmail.com>
2026-09-17 16:35:15 -0600

    Point API base at Fly.io backend

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://newspaper-backend-w3kl.onrender.com/api";
+const API_BASE = "https://paper-of-love-backend.fly.dev/api";
 
 async function apiRequest(path, options) {
   const res = await fetch(`${API_BASE}${path}`, {
